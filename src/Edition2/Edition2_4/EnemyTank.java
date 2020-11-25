8701695b4f5ff58aff147f1ff93ecba9fd214c13
Wmt-Monica <3040988158@qq.com>
2020-11-25 23:44:41 +0800

    2020/11/25 尝试实战制作Java项目一：飞机大战 /**  * 版本2.4  * 功能：  *   添加主战坦克的生命血条  */

---
+++ src/Edition2/Edition2_4/EnemyTank.java
@@ -0,0 +1,212 @@
+package Edition2.Edition2_4;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 版本2.4
+ * 功能：
+ *   添加主战坦克的生命血条
+ */
+
+// 敌方坦克容器类对象
+public class EnemyTank implements Runnable {
+    static int enemyTankNum = 0;
+    private List<Tank> tankList = Collections.synchronizedList(new ArrayList<>());  // 创建一个存放敌方坦克的容器
+    private List<Bullet> bulletList = new ArrayList<>();  //创建子弹容器类对象
+    private Tank myTank = null;  // 创建主战坦克对象
+    private static int SCREEN_WIDTH = 800;  // 游戏窗口的宽度
+    private static int SCREEN_HEIGHT = 600;  // 游戏窗口的高度
+    private TankClient tankClient;
+
+    // 构造器
+    public EnemyTank(Tank tank, TankClient tankClient){
+        this.myTank = tank;
+        this.tankClient = tankClient;
+    }
+
+    public List<Bullet> getBulletList() {
+        return bulletList;
+    }
+
+    public List<Tank> getTankList() {
+        return tankList;
+    }
+
+    public static int getEnemyTankNum() {
+        return enemyTankNum;
+    }
+
+    @Override
+    public void run() {
+        creatEnemyTank();
+    }
+
+    //批量创建敌方坦克的方法
+    public void creatEnemyTank(){
+
+        int x;
+        int y;
+        if (enemyTankNum < 10){  //如果游戏界面上敌方坦克的个数少于10个就创建新的敌方坦克
+            x = new Random().nextInt(SCREEN_WIDTH - 40 - 10 + 1)+10;
+            y = new Random().nextInt(SCREEN_HEIGHT - 40 - 30 + 1)+30;
+            //随机获取敌方坦克的位置
+            while (!(Math.abs(x - myTank.getX()) <= 200) && !(Math.abs(y - myTank.getY()) <= 200)
+                || (x + myTank.getTANK_SIZE() >= tankClient.getWall1().getX() && x <= tankClient.getWall1().getX()+tankClient.getWall1().getWeight()
+                && y + myTank.getTANK_SIZE() >= tankClient.getWall1().getY() && y <= tankClient.getWall1().getY()+tankClient.getWall1().getHeight())
+                    || (x + myTank.getTANK_SIZE() >= tankClient.getWall2().getX() && x <= tankClient.getWall2().getX()+tankClient.getWall2().getWeight()
+                    && y + myTank.getTANK_SIZE() >= tankClient.getWall2().getY() && y <= tankClient.getWall2().getY()+tankClient.getWall2().getHeight())){
+                 x = new Random().nextInt(SCREEN_WIDTH - 40 - 10 + 1)+10;
+                 y = new Random().nextInt(SCREEN_HEIGHT - 40 - 30 + 1)+30;
+            }
+            synchronized (tankList){
+                tankList.add(new Tank(x,y,false,tankClient));
+            }
+            enemyTankNum++;
+        }
+
+    }
+
+    // 创建批量画出敌方坦克的方法
+    public void drawTanks(Graphics g){
+        if (enemyTankNum > 0){
+            new ergodicEnemyTankList(tankList,g).start();
+            new removeEnemyTank(tankList).start();
+        }
+    }
+
+    // 遍历敌方坦克容器的类
+    public class ergodicEnemyTankList extends Thread {
+
+        Graphics g;
+
+        List<Tank> tankList;
+        public ergodicEnemyTankList(List<Tank> tankList, Graphics g){
+            this.tankList = tankList;
+            this.g = g;
+        }
+
+        @Override
+        public void run() {
+            synchronized (tankList){
+                if (enemyTankNum < 10){
+                    creatEnemyTank();
+                }
+                int i = 0;
+                for (Tank tank : tankList){
+                    i ++;
+                    if (tank.getBLive()){
+                        enemyTankAction(tank);
+                        new fire(tank,g).start();
+                        Color bulletColor = g.getColor();
+                        g.setColor(Color.BLUE);
+                        synchronized (tank){
+                            if (i < 10){
+                                tank.OldX = tank.getX();
+                                tank.OldY = tank.getY();
+                                tank.draw(g);
+                            }else {
+                                tankList.remove(tank);
+                            }
+                        }
+                        g.setColor(bulletColor);
+                    }
+                }
+            }
+        }
+    }
+
+
+    // 移除已经死亡的坦克
+    public class removeEnemyTank extends Thread{
+
+        List<Tank> tankList;
+        public removeEnemyTank(List<Tank> tankList){
+            this.tankList = tankList;
+        }
+
+        @Override
+        public void run() {
+            for (Tank tank : tankList){
+                if (!tank.getBLive()){
+                    tankList.remove(tank);
+                    enemyTankNum--;
+                }
+            }
+        }
+    }
+
+    // 创造敌方坦克转换方向的方法
+    public Tank.DirectionENUM enemyTankAction(Tank enemyTank){
+
+        // 设置该坦克的最少移动多少步数才尝试改变移动方向
+        if (enemyTank.time < 100){
+            enemyTank.fireTime = 0;
+            enemyTank.time ++;
+            return enemyTank.bulletDirection;  // 返回原来的方向
+        }else {
+            enemyTank.time = 0;
+            enemyTank.fireTime = 1;
+        }
+
+        enemyTank.step = new Random().nextInt(20)+5;
+        synchronized (this){
+            // 创建移动方向数组
+            Tank.DirectionENUM[] enemyDirections = {
+                    Tank.DirectionENUM.U, Tank.DirectionENUM.UR, Tank.DirectionENUM.R,
+                    Tank.DirectionENUM.RD, Tank.DirectionENUM.D, Tank.DirectionENUM.LD,
+                    Tank.DirectionENUM.L, Tank.DirectionENUM.LU, Tank.DirectionENUM.STOP};
+
+            enemyTank.bulletDirection= enemyDirections[new Random().nextInt(9)];  // 随机产生[0,9)之间的方向数组对应的下标的数字
+            enemyTank.tankMove(enemyTank.bulletDirection);
+        }
+        if (enemyTank.step == 0 ){
+            enemyTank.step = new Random().nextInt(20)+5;
+        }else {
+            enemyTank.step--;
+            enemyTank.tankMove(enemyTank.bulletDirection);
+        }
+        return enemyTank.bulletDirection;
+    }
+
+    // 创建坦克是否越界的方法
+    public boolean crossScreen(int nowX,int nowY){
+        if (nowX < 10 || nowX > new TankClient().getScreenWidth()-40 || nowY < 30 || nowY > new TankClient().getScreenHeight()-40){
+            return false;
+        }
+        return true;
+    }
+
+    // 创建发射子弹的方法
+    class fire extends Thread{
+        Tank tank = null;
+        Graphics g;
+        public fire(Tank tank, Graphics g){
+            this.tank = tank;
+            this.g = g;
+        }
+        @Override
+        public void run() {
+            if (tank.fireTime == 1){
+                Bullet bullet = enemyTankFire();
+                if (bulletList.contains(tank)){
+                    bullet.draw(g);
+                }
+            }
+        }
+
+        public Bullet enemyTankFire(){
+            Bullet bullet = new Bullet(
+                    tank.getX()+(tank.getTankSize()-new Bullet().getBULLET_SIZE())/2,
+                    tank.getY()+(tank.getTankSize()-new Bullet().getBULLET_SIZE())/2,
+                    tank.bulletDirection,false); // 根据创建的坦克类，给该坦克类创建子弹类
+            bulletList.add(bullet);
+            return bullet;
+        }
+    }
+
+
+}
---
+++ src/Edition2/Edition2_4/Explode.java
@@ -0,0 +1,52 @@
+package Edition2.Edition2_4;
+
+import java.awt.*;
+
+/**
+ * 版本2.4
+ * 功能：
+ *   添加主战坦克的生命血条
+ */
+
+//爆炸类
+public class Explode {
+    int x,y;   // 爆炸的x,y坐标位置
+    private static boolean bLive = true;  // 爆炸体是否存活
+    int[] ExplodeSize = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,38,36,34,32,30,28,26,
+    24,22,20,18,16,14,12,10,8,6,4,2};  // 爆炸体实心圆的直径
+    private int time = 0;  // 爆炸体默认从显示实心圆直径的第一个开始
+    Tank tank;  // 爆炸的坦克对象
+
+    // 有参构造器
+    public Explode(int x, int y, Tank tank) {
+        this.x = x;
+        this.y = y;
+        this.tank = tank;
+    }
+
+    // 无参构造器
+    public Explode(){
+
+    }
+
+    //爆炸体绘画自己的方法
+    public void draw(Graphics g) {
+
+        while (bLive){  // 如果爆炸体还存活就画出
+            Color tankColor = g.getColor();
+            // 画出实心圆爆炸类对象
+            g.setColor(Color.ORANGE);
+            g.fillOval(x, y , ExplodeSize[time],ExplodeSize[time]);
+            g.setColor(tankColor);
+            time++;
+
+            //如果该爆炸体已经全部画完就将其各参数恢复默认，爆炸体已死亡
+            if (time == ExplodeSize.length-1){
+                bLive = false;
+            }
+        }
+        bLive = true;
+    }
+}
+
+
