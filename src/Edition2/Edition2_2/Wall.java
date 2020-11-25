f6a5e1f4c2b4389c6eea65e5c77ac005d5266b30
Wmt-Monica <3040988158@qq.com>
2020-11-25 19:21:02 +0800

    2020/11/25 尝试实战制作Java项目一：飞机大战 /**  * 版本2.2  * 功能：  *   添加主战坦克的超级炮弹  */

---
+++ src/Edition2/Edition2_2/Wall.java
@@ -0,0 +1,44 @@
+package Edition2.Edition2_2;
+
+import java.awt.*;
+
+/**
+ * 版本2.2
+ * 功能：
+ *   添加主战坦克的超级炮弹
+ */
+public class Wall {
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    private int y;  // 墙的位置
+    private int weight, height;  // 墙的宽高
+    private TankClient tankClient;  // 主界面对象
+
+    public Wall(int x, int y, int weight, int height, TankClient tankClient) {
+        this.x = x;
+        this.y = y;
+        this.weight = weight;
+        this.height = height;
+        this.tankClient = tankClient;
+    }
+
+    public void draw(Graphics g){
+        g.fillRect(x,y,weight,height);  // 画出实心长方形的墙
+    }
+}
