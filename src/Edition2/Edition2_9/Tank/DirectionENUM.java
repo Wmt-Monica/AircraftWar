c33fd833729024ba27a8de36532de12adb6a8539
Wmt-Monica <3040988158@qq.com>
2020-11-26 23:00:20 +0800

    2020/11/26 尝试实战制作Java项目一：飞机大战 飞机大战终结版，并生成了关于此的API文档

--- src/Edition2/Edition2_9/Tank/DirectionENUM.java
+++ src/Edition2/Edition2_9/Tank/DirectionENUM.java
@@ -4,16 +4,5 @@ package Edition2.Edition2_9.Tank;
  * 各个坦克大战中坦克和炮弹的移动方向枚举
  */
 public enum DirectionENUM {
-    /**
-     * @param  U 方向：上
-     * @param  UR 方向：右上
-     * @param  R 方向：向右
-     * @param  RD 方向：右下
-     * @param  D 方向：下
-     * @param  LD 方向：左下
-     * @param  L 方向：左
-     * @param  LU 方向：左上
-     * @param  STOP 方向：静止
-     */
     U, UR, R, RD, D, LD, L, LU, STOP
 }
